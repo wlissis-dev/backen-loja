b58c314d4bf3176da20a556e64d950039fee19e5
Wlissis <wlissistech@gmail.com>
2026-01-20 10:59:49 -0300

    Feat:Rota de listagem de itens criada

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,9 @@ import type { Item } from './types.js';
 const app = express();
 app.use(express.json());
 
-let iventario = []
+let iventario : Item[] = []
 
+//Rota para criar um item novo
 app.post("/itens", (req:Request, res: Response)=>{
     const {id, nome, categoria, preco, estoque} = req.body;
     const novoItem : Item  = {
@@ -21,7 +22,22 @@ app.post("/itens", (req:Request, res: Response)=>{
     return res.status(201).json(novoItem)
 })
 
+//Rota para listar todos os itens
+app.get("/itens", (req:Request, res:Response)=>{
+    try {
+        if(iventario.length === 0){
+            console.log("Consulta realizada: Iventario vazio")
+            return res.json([])
+        }
+         console.log(`Conculta realizada: Retornando ${iventario.length} Itens`)
+         return res.json(iventario)
+    } catch (error) {
+        console.error("Erro ao exibir dados:", error)
+        return res.status(500).json({mensagem:"Erro interno no serivor"})
+    }
+})
+
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`🚀 Servidor TypeScript rodando em http://localhost:${PORT}`);
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
